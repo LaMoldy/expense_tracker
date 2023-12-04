c6832a98593061d8bb49d697f0e9b073966c8279
LaMoldy <NikkolasJ@Outlook.com>
2023-12-04 18:27:13 -0500

    feat: added two buttons for imorting an exporting data

---
+++ src/components/FileInputButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="input-container">
+    <p class="text-center">{{ option }}</p>
+    <label for="import">
+      <input name="import" id="import" type="file" />
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FormButton',
+  props: {
+    option: String,
+  },
+});
+</script>
+
+<style lang="scss">
+#input-container {
+  width: 250px;
+  padding: 10px;
+  border-radius: 10px;
+  border: 0 solid transparent;
+  font-size: 1em;
+  background-color: #0059ff;
+  color: white;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #3e82ff;
+  }
+}
+
+label {
+  display: none;
+}
+
+p {
+  font-size: 1em;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -3,6 +3,8 @@
     <div class="flex-col flex-center" id="settings-content-container">
       <h1 class="header">Settings</h1>
       <ThemeToggle />
+      <FileInputButton option="Import Data" v-bind:on-click="importData" />
+      <FileInputButton option="Export Data" />
       <WarningButton text="Clear Data" />
     </div>
     <MenuButton text="Back" buttonValue="backSettings" />
@@ -14,16 +16,34 @@ import { defineComponent } from 'vue';
 import ThemeToggle from '@/components/ThemeToggle.vue';
 import MenuButton from '@/components/MenuButton.vue';
 import WarningButton from '@/components/WarningButton.vue';
+import FileInputButton from '@/components/FileInputButton.vue';
+
+function importData() {
+  return 0;
+}
+
+function exportData() {
+  return 0;
+}
 
 export default defineComponent({
   name: 'SettingsView',
-  components: { MenuButton, ThemeToggle, WarningButton },
+  components: {
+    MenuButton,
+    ThemeToggle,
+    WarningButton,
+    FileInputButton,
+  },
+  methods: {
+    importData,
+    exportData,
+  },
 });
 </script>
 
 <style lang="scss" scoped>
 #settings-container {
-  gap: 200px;
+  gap: 125px;
 }
 
 #settings-content-container {
